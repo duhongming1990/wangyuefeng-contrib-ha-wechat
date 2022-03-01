e0aa5b08567d99508e11bf20e15c706a2263ab84
shaonianzhetnan <5294022+shaonianzhentan@users.noreply.github.com>
2022-03-01 13:02:11 +0800

    增加GPS我的位置功能

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 [![home-assistant](https://img.shields.io/badge/Home-Assistant-%23049cdb)](https://www.home-assistant.io/)
 [![NPM version](https://img.shields.io/npm/v/node-red-contrib-ha-wechat.svg?style=flat-square)](https://www.npmjs.com/package/node-red-contrib-ha-wechat)
 
-![visit](https://visitor-badge.glitch.me/badge?page_id=shaonianzhentan.node-red-contrib-ha-wechat&left_text=visit)
+![visit](https://visitor-badge.laobi.icu/badge?page_id=shaonianzhentan.node-red-contrib-ha-wechat&left_text=visit)
 ![forks](https://img.shields.io/github/forks/shaonianzhentan/node-red-contrib-ha-wechat)
 ![stars](https://img.shields.io/github/stars/shaonianzhentan/node-red-contrib-ha-wechat)
 ![license](https://img.shields.io/github/license/shaonianzhentan/node-red-contrib-ha-wechat)
@@ -28,6 +28,8 @@ MQTT服务：broker-cn.emqx.io
 MQTT端口：1883
 ```
 
+**友情提醒：如果实在不会部署，付费咨询请加Q`635147515`**
+
 ## 功能截图
 
 ![img](https://cdn.jsdelivr.net/gh/shaonianzhentan/image@main/node-red-contrib-ha-wechat/1.png)
@@ -36,6 +38,8 @@ MQTT端口：1883
 
 ## 更新日志
 
+### 1.0.2
+- 增加GPS持续定位功能
 ### 1.0.1
 - 修复主题订阅失败问题
 - camera支持图文消息返回
--- gps/gps.js
+++ gps/gps.js
@@ -11,29 +11,65 @@ module.exports = function (RED) {
             server.register(this)
             // 初始HA连接
             let ha = null
-            if (hassUrl && hassToken && hassUser){
+            if (hassUrl && hassToken && hassUser) {
                 ha = new HomeAssistant({ hassUrl, hassToken })
-            }            
+            }
             // 监听GPS上报信息
             server.subscribe(topic, { qos: 0 }, async function (mtopic, mpayload, mpacket) {
                 const payload = mpayload.toString()
                 // console.log(payload)
                 try {
                     const message = JSON.parse(CryptoUtil.DecryptGPSText(payload, uid))
-                    node.send({ payload: message })
-                    node.status({ fill: "green", shape: "ring", text: `经度：${message.lng}  纬度：${message.lng}` });
-                    if(ha){
-                        try{
+                    node.status({ fill: "green", shape: "ring", text: `经纬度：${message.Lat}，${message.Lnt}` });
+                    if (ha) {
+                        try {
                             const updateUserApi = `states/${hassUser}`
                             let { state, attributes } = await ha.getApi(updateUserApi)
+                            // 默认使用小写
+                            let latField = 'Latitude' in attributes ? 'Latitude' : 'latitude'
+                            let lntField = 'Longitude' in attributes ? 'Longitude' : 'longitude'
+                            // 相同则不更新
+                            if (attributes[latField] == message.Lat && attributes[lntField] == message.Lnt) {
+                                node.send({ payload: message })
+                                return
+                            }
+                            attributes[latField] = message.Lat
+                            attributes[lntField] = message.Lnt
                             await ha.postApi(updateUserApi, { state, attributes })
-                            node.status({ fill: "green", shape: "ring", text: `${attributes.friendly_name}位置更新成功，当前状态：${state}` });
-                        }catch(ex){
+                            // 读取历史记录
+                            const today = new Date()
+                            const diffTime = 1
+                            today.setHours(today.getHours() - diffTime)
+                            const startTime = today.toISOString()
+                            today.setHours(today.getHours() + diffTime)
+                            const endTime = today.toISOString()
+                            let history = await ha.getApi(`history/period/${startTime}?end_time=${endTime}&filter_entity_id=${hassUser}`)
+                            if (history.length > 0) {
+                                history = history[0].map(ele => {
+                                    return {
+                                        friendly_name: ele.attributes.friendly_name,
+                                        state: ele.state,
+                                        latitude: ele.attributes[latField],
+                                        longitude: ele.attributes[lntField]
+                                    }
+                                })
+                            }
+                            node.send({
+                                payload: message,
+                                history: {
+                                    start: startTime,
+                                    end: endTime,
+                                    list: history
+                                }
+                            })
+                            return
+                        } catch (ex) {
                             node.status({ fill: "red", shape: "ring", text: JSON.stringify(ex) });
                         }
                     }
+                    node.send({ payload: message })
                 } catch (ex) {
-                    console.log(ex)
+                    // console.log(ex)
                 }
             })
         } else {
